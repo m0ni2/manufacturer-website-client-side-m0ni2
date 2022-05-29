ae86a342f7ed6cad8bd02a3f92811fd436e6f9dd
Md. Monirul Islam <monir.rj@gmail.com>
2022-05-29 20:04:30 +0600

    firebase hooks, axios install

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Header from './Pages/Shared/Header';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/termsOfService' element={<Home />} />
+        <Route path='/privacyPolicy' element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -6,20 +6,20 @@ import Logo from '../../assets/images/logo.png';
 const Header = () => {
     return (
         <header className='sticky-top'>
-            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" sticky='top'>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky='top'>
                 <Container>
                     <Navbar.Brand as={Link} to='/'>
                         <img src={Logo} className='header-img' alt="logo" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                            <Nav.Link as={Link} to='/about'>About</Nav.Link>
-                            <Nav.Link as={Link} to='/checkout'>Checkout</Nav.Link>
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                            <Nav.Link as={Link} to='/'>About</Nav.Link>
+                            <Nav.Link as={Link} to='/'>Checkout</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to='/register'>Register</Nav.Link>
+                            <Nav.Link as={Link} to='/'>Register</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
