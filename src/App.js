e79b01b0e15cd1e4d756c389bfbcdacf3b6eaac2
Md. Monirul Islam <monir.rj@gmail.com>
2022-05-29 22:46:50 +0600

    reInstall tailwind

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Header from './Pages/Shared/Header';
-import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/termsOfService' element={<Home />} />
-        <Route path='/privacyPolicy' element={<Home />} />
-      </Routes>
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
