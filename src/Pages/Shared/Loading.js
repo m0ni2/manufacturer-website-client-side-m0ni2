3c30146f874bdfee9384f6672b224dcb0706dfa0
Md. Monirul Islam <monir.rj@gmail.com>
2022-05-30 23:50:45 +0600

    loading fixed

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className='container py-12'>
-            <div className="radial-progress bg-primary text-primary-content border-4 border-primary" style={{ '--value': '70' }}>70%</div>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md mt-[45vh]">
+                    <progress className="progress w-56"></progress>
+                </div>
+            </div>
         </div>
     );
 };
